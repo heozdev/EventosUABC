3a82eb02c828a6d93c49a15489fd17d0cab4f9d5
Guevara <themuppet2003@gmail.com>
2024-03-28 19:58:33 -0700

    commit general

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,3 +1,38 @@
+import {
+    Box,
+    Button,
+    Container,
+    FormControl,
+    FormLabel,
+    Heading,
+    Input,
+    Stack,
+} from "@chakra-ui/react";
+
 export const Login = () => {
-    return <div>Login</div>;
+
+    return (
+        <Container borderRadius={10} p={10} mt={20} boxShadow={"xl"}>
+            <Heading mb={20}>Login</Heading>
+            <FormControl>
+                <Stack spacing={10}>
+                    <Box>
+                        <FormLabel>Correo</FormLabel>
+                        <Input type="email" placeholder="Ingrese su correo" />
+                    </Box>
+                    <Box>
+                        <FormLabel>Contraseña</FormLabel>
+                        <Input
+                            type="password"
+                            placeholder="Ingrese su contraseña"
+                        />
+                    </Box>
+                    <Box>
+                        <Button w={"100%"}>Iniciar sesion</Button>
+                    </Box>
+                </Stack>
+                <Stack></Stack>
+            </FormControl>
+        </Container>
+    );
 };
