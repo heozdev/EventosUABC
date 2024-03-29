b78ad740cca994767018be50f3108108f13dad41
hector <hosuna2900@gmail.com>
2024-03-28 18:28:05 -0700

    Commit con la interfaz del login

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,3 +1,34 @@
+import {
+    Box,
+    Button,
+    Container,
+    FormControl,
+    FormLabel,
+    Heading,
+    Input,
+    Stack,
+} from "@chakra-ui/react";
+
 export const Login = () => {
-    return <div>Login</div>;
+    return (
+        <Container borderRadius={10} p={10} mt={20} boxShadow={"xl"}>
+            <Heading mb={20}>Login</Heading>
+            <FormControl>
+                <Stack spacing={10}>
+                    <Box>
+                        <FormLabel>Correo</FormLabel>
+                        <Input type="email" />
+                    </Box>
+                    <Box>
+                        <FormLabel>Contraseña</FormLabel>
+                        <Input type="password" />
+                    </Box>
+                    <Box>
+                        <Button w={"100%"}>Iniciar sesion</Button>
+                    </Box>
+                </Stack>
+                <Stack></Stack>
+            </FormControl>
+        </Container>
+    );
 };
