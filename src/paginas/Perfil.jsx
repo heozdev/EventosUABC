a64fea715c266d28c9ebdc7d65c77e534ba861da
Guevara <themuppet2003@gmail.com>
2024-03-31 01:01:23 -0700

    Actulizar MostrarEventos

--- src/paginas/Perfil.jsx
+++ src/paginas/Perfil.jsx
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 import { Container, Grid, GridItem, Heading } from "@chakra-ui/react";
-import PanelSolicitud from "../componentes/Solicitud/PanelSolicitud";
 import { InformacionPerfil } from "../componentes/Perfil/InformacionPerfil";
 import { AccionesPerfil } from "../componentes/Perfil/AccionesPerfil";
 
@@ -21,14 +19,5 @@ export const Perfil = () => {
                 </Grid>
             </Container>
         </>
-=======
-import PanelSolicitud from "../componentes/Solicitud/PanelSolicitud";
-
-export const Perfil = () => {
-    return (
-        <div>
-            <PanelSolicitud/>
-        </div>
->>>>>>> 295e8db07bfc0fc2a933ad7f1d7a65c0215f10be
     );
 };
