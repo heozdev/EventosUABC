53363aeafc0a964960930ca18fd2a7181529deec
canettgomez <leoj.canett22@gmail.com>
2024-04-04 18:50:31 -0700

    ControladorSolicitud

---
+++ src/controladores/Solicitud/ControladorSolicitud.js
@@ -0,0 +1,16 @@
+
+const agregarSolicitud=(PanelSolicitud)=>{
+    
+}
+
+const eliminarSolicitud =(PanelSolicitud) =>{
+
+}
+
+const editarSolicitud= (PanelSolicitud) =>{
+
+}
+
+const consultarSolicitud = (PanelSolicitud) => {
+
+} 
