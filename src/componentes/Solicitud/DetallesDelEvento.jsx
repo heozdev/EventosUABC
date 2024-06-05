a5971f71a1b540dec71acaa321e8bebf980894ed
Guevara <125514314+Alberto-Guevara@users.noreply.github.com>
2024-06-05 16:30:05 -0700

    Modificacion en la visualizacion de los detalles de los eventos

--- src/componentes/Solicitud/DetallesDelEvento.jsx
+++ src/componentes/Solicitud/DetallesDelEvento.jsx
@@ -9,7 +9,14 @@ import {
     Badge,
     Button,
     useToast,
+    Image,
+    Grid,
+    GridItem,
+    Center,
+    FormControl,
+    FormLabel,
 } from "@chakra-ui/react";
+import { format } from "date-fns";
 
 export const DetallesDelEvento = () => {
     const { id } = useParams();
@@ -41,52 +48,98 @@ export const DetallesDelEvento = () => {
 
     return (
         <Box p={8}>
-            <VStack spacing={4} align="stretch">
-                <Heading as="h1" size="xl">
+            <Heading as="h1" size="xl" textAlign={"center"}>
                     {evento.solicitud.nombre}
                 </Heading>
-                <Text fontSize="xl">{evento.solicitud.descripcion}</Text>
-                <HStack>
-                    <Text fontWeight="bold">Fecha:</Text>
-                    <Text>{evento.solicitud.fecha}</Text>
-                </HStack>
-                <HStack>
-                    <Text fontWeight="bold">Hora:</Text>
-                    <Text>
-                        {evento.solicitud.horaInicio} - {evento.solicitud.horaFin}
-                    </Text>
-                </HStack>
-                <HStack>
-                    <Text fontWeight="bold">Ubicación:</Text>
-                    <Text>{evento.solicitud.ubicacion?.direccion}</Text>
-                </HStack>
-                <HStack>
-                    <Text fontWeight="bold">Capacidad:</Text>
-                    <Text>{evento.solicitud.capacidad}</Text>
-                </HStack>
-                <HStack>
-                    <Text fontWeight="bold">Responsable:</Text>
-                    <Text>{evento.solicitud.responsable}</Text>
-                </HStack>
-                <HStack>
-                    <Text fontWeight="bold">Modalidad:</Text>
-                    <Text>{evento.solicitud.modalidad}</Text>
-                </HStack>
-                <HStack>
-                    <Text fontWeight="bold">Valor en Créditos:</Text>
-                    <Text>{evento.solicitud.valorEnCreditos ? "Sí" : "No"}</Text>
-                </HStack>
-                <HStack>
-                    <Text fontWeight="bold">Total de Sellos:</Text>
-                    <Text>{evento.solicitud.totalSellos}</Text>
-                </HStack>
-                <Badge colorScheme="green" fontSize="lg">
-                    {evento.estado}
-                </Badge>
-                <Button colorScheme="blue" size="lg">
-                    Registrarse
+                <Center>
+                    <VStack display="grid" gridTemplateColumns={"1fr 1fr"} align="stretch" mt={10} width={"50%"}>
+                        <Image
+                            src="/src/recursos/imagenes/ejemploEvento.jpg"
+                            alt="Imagen del evento"
+                            objectFit="cover"
+                            width="70%"
+                            height="200px"
+                            borderRadius="md"
+                        />
+                        <Text fontSize="xl" textAlign={"justify"}>{evento.solicitud.descripcion}</Text>
+                    </VStack>
+                </Center>
+            <Center>
+                <Grid templateColumns="repeat(2, 1fr)" gap={4} width={"50%"} mt={10}>
+                    <FormControl>
+                        <FormLabel mt={3} fontSize="m">
+                            <b>Responsable: </b>
+                            {evento.solicitud.responsable}
+                        </FormLabel>
+                        <FormLabel mt={3} fontSize="m">
+                            <b>Modalidad: </b>
+                            {evento.solicitud.modalidad}
+                        </FormLabel>
+                        <FormLabel mt={3} fontSize="m">
+                            <b>Capacidad: </b>
+                            {evento.solicitud.capacidad}
+                        </FormLabel>
+                        <FormLabel mt={3} fontSize="m">
+                            <b>Valor en Créditos: </b>
+                            {evento.solicitud.valorEnCreditos ? "Sí" : "No"}
+                        </FormLabel>
+                        <FormLabel mt={3} fontSize="m">
+                            <b>Total de Sellos: </b>
+                            {evento.solicitud.totalSellos}
+                        </FormLabel>
+                        <FormLabel mt={3} fontSize="m">
+                            <b>Hora Inicio: </b>
+                            {evento.solicitud.horaInicio}
+                        </FormLabel>
+                        <FormLabel mt={3} fontSize="m">
+                            <b>Hora Fin: </b>
+                            {evento.solicitud.horaFin}
+                        </FormLabel>
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel mt={3} fontSize="m">
+                            <b>Facultad: </b>
+                            {evento.solicitud.ubicacion.facultad}
+                        </FormLabel>
+                        <FormLabel mt={3} fontSize="m">
+                            <b>Estado: </b>
+                            {evento.solicitud.ubicacion.estado}
+                        </FormLabel>
+                        <FormLabel mt={3} fontSize="m">
+                            <b>Campus: </b>
+                            {evento.solicitud.ubicacion.campus}
+                        </FormLabel>
+                        <FormLabel mt={3} fontSize="m">
+                            <b>Ciudad: </b>
+                            {evento.solicitud.ubicacion.ciudad}
+                        </FormLabel>
+                        <FormLabel mt={3} fontSize="m">
+                            <b>Dirección: </b>
+                            {evento.solicitud.ubicacion.direccion}
+                        </FormLabel>
+                        <FormLabel mt={3} fontSize="m">
+                            <b>Aula: </b>
+                            {evento.solicitud.ubicacion.aula}
+                        </FormLabel>
+                        <FormLabel mt={3} fontSize="m">
+                            <b>Fecha: </b>
+                            {evento.solicitud.fecha}
+                        </FormLabel>
+                        <FormLabel mt={3} fontSize="m">
+                            <b>Fecha de envio: </b>
+                            {format(
+                                new Date(evento.solicitud.fecha),
+                                "dd/MM/yyyy HH:mm:ss"
+                            )}
+                        </FormLabel>
+                    </FormControl>
+                </Grid>
+            </Center>
+            <Center>
+                <Button colorScheme="green" size="lg" mt={10}>
+                    Asistiré
                 </Button>
-            </VStack>
+            </Center>
         </Box>
     );
 };
