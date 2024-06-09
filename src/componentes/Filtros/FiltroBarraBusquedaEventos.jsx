7e0ff588ecd6b5820ff7da2fedac21c8a97d6002
Edivanse <sebastian.sandoval@uabc.edu.mx>
2024-06-08 19:41:34 -0700

    Mostrar eventos y solicitudes
    Se codificó un botón para mostrar todas las solicitudes y todos los eventos en la barra de búsqueda

--- src/componentes/Filtros/FiltroBarraBusquedaEventos.jsx
+++ src/componentes/Filtros/FiltroBarraBusquedaEventos.jsx
@@ -15,77 +15,90 @@ import {
     Button } 
     from '@chakra-ui/react';
 
-const FiltroBarraBusquedaEventos = ({ isOpenModalSearch, onCloseModalSearch, eventos, setEventos }) => {
-    const [search, setSearch] = useState("");
-    const [showAlert, setShowAlert] = useState(false);
-
-    useEffect(() => {
-        setSearch(""); // Restablecer el estado search cuando isOpenModalSearch cambia
-        setShowAlert(false);
-    }, [isOpenModalSearch]);
-
-    const handleChangeSearch = (e) => {
-        setSearch(e.target.value);
-    };
-
-    const handleSearch = () => {
-        console.log("Eventos originales:", eventos);
-        const filtered = eventos.filter(evento => {
-            const nombreIncluido = evento.solicitud.nombre.toLowerCase().includes(search.toLowerCase());
-            const facultadIncluida = evento.solicitud.ubicacion.facultad.toLowerCase().includes(search.toLowerCase());
-            const ciudadIncluida = evento.solicitud.ubicacion.ciudad.toLowerCase().includes(search.toLowerCase());
-
-            return nombreIncluido || ciudadIncluida || facultadIncluida;
-        });
-
-        console.log("Eventos filtrados:", filtered);
-
-        if (filtered.length === 0) {
-            setShowAlert(true);
-        } else {
+    const FiltroBarraBusquedaEventos = ({ isOpenModalSearch, onCloseModalSearch, eventos, setEventos }) => {
+        const [search, setSearch] = useState("");
+        const [showAlert, setShowAlert] = useState(false);
+        const [originalEventos, setOriginalEventos] = useState([]);
+    
+        useEffect(() => {
+            setSearch(""); // Restablecer el estado search cuando isOpenModalSearch cambia
+            setShowAlert(false);
+            if (originalEventos.length === 0) {
+                setOriginalEventos(eventos); // Guardar los eventos originales si aún no se han guardado
+            }
+        }, [isOpenModalSearch, eventos, originalEventos]);
+    
+        const handleChangeSearch = (e) => {
+            setSearch(e.target.value);
+        };
+    
+        const handleSearch = () => {
+            console.log("Eventos originales:", originalEventos);
+            const filtered = originalEventos.filter(evento => {
+                const nombreIncluido = evento.solicitud.nombre.toLowerCase().includes(search.toLowerCase());
+                const facultadIncluida = evento.solicitud.ubicacion.facultad.toLowerCase().includes(search.toLowerCase());
+                const ciudadIncluida = evento.solicitud.ubicacion.ciudad.toLowerCase().includes(search.toLowerCase());
+    
+                return nombreIncluido || ciudadIncluida || facultadIncluida;
+            });
+    
+            console.log("Eventos filtrados:", filtered);
+    
+            if (filtered.length === 0) {
+                setShowAlert(true);
+            } else {
+                setShowAlert(false);
+                setEventos(filtered);
+                onCloseModalSearch();
+            }
+        };
+    
+        const handleShowAll = () => {
+            setEventos(originalEventos); // Restablecer los eventos al estado original
             onCloseModalSearch();
-            setEventos(filtered);
-        }
+        };
+    
+        const handleCloseModal = () => {
+            setShowAlert(false);
+            onCloseModalSearch();
+        };
+    
+        return (
+            <>
+                <Modal isOpen={isOpenModalSearch} onClose={handleCloseModal}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>Barra de búsqueda de eventos</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <FormControl>
+                                <FormLabel>Ingresa alguna palabra clave</FormLabel>
+                                <Input
+                                    placeholder='Facultad, ciudad, evento'
+                                    type='text'
+                                    value={search}
+                                    onChange={handleChangeSearch}
+                                />
+                            </FormControl>
+                            {showAlert && (
+                                <Alert status="warning" mt={4}>
+                                    <AlertIcon />
+                                    No se encontraron resultados.
+                                </Alert>
+                            )}
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button colorScheme='green' mr={3} onClick={handleSearch}>
+                                Buscar
+                            </Button>
+                            <Button colorScheme='green' onClick={handleShowAll}>
+                                Mostrar todos
+                            </Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+            </>
+        );
     };
-
-    const handleCloseModal = () => {
-        setShowAlert(false);
-        onCloseModalSearch();
-    };
-
-    return (
-        <>
-            <Modal isOpen={isOpenModalSearch} onClose={handleCloseModal}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Barra de búsqueda de eventos</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <FormControl>
-                            <FormLabel>Ingresa alguna palabra clave</FormLabel>
-                            <Input
-                                placeholder='Facultad, ciudad, evento'
-                                type='text'
-                                value={search}
-                                onChange={handleChangeSearch}
-                            />
-                        </FormControl>
-                        {showAlert && (
-                            <Alert status="warning" mt={4}>
-                                <AlertIcon />
-                                No se encontraron resultados.
-                            </Alert>
-                        )}
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button colorScheme='green' mr={3} onClick={handleSearch}>
-                            Buscar
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </>
-    );
-};
-
-export default FiltroBarraBusquedaEventos;
+    
+    export default FiltroBarraBusquedaEventos;
